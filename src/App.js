353ff9e49d57ece0de2cfe082c2d04687ecea8a5
Akshit Patel <aksh1150@gmail.com>
2020-10-10 18:19:26 -0400

    Crate auth.service file and setup register, login, logout, and getCurrentUser functional components

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+import Container from "@material-ui/core/Container";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Container maxWidth="sm">
+        <h1>Hello</h1>
+      </Container>
+    </div>
+  );
+}
+
+export default App;
